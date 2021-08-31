18b079d629052421caf2c5edf4e9ced5fb66a74a
Edris Ahani <edris.ahani.tutorial@gmail.com>
2021-08-31 10:14:08 +0430

    session7

--- session7/src/main/java/Main.java
+++ session7/src/main/java/Main.java
@@ -5,13 +5,10 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("ApplicationContext.xml");
-        Employee e = (Employee) context.getBean("e");
-        e.show();
 
         Employee e1 = (Employee) context.getBean("e1");
         e1.show();
 
-        Employee e2 = (Employee) context.getBean("e2");
-        e2.show();
+
     }
 }
