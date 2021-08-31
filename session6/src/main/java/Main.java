353897724f88378d2cc1bb91737f195e7c2daaef
Edris Ahani <edris.ahani.tutorial@gmail.com>
2021-08-31 10:12:14 +0430

    session6

---
+++ session6/src/main/java/Main.java
@@ -0,0 +1,17 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("ApplicationContext.xml");
+        Employee e1 = (Employee) context.getBean("e1");
+        e1.show();
+
+        Employee e2 = (Employee) context.getBean("e2");
+        e2.show();
+
+        Employee e3 = (Employee) context.getBean("e3");
+        e3.show();
+    }
+}
